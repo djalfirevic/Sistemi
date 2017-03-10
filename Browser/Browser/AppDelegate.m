2c55beab71aa2d01f4620c03c89b9e5657e82cc1
Djuro Alfirevic <djuro.alfirevic@symphony.is>
2017-03-10 20:45:30 +0100

    Browser project added.

---
+++ Browser/Browser/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  Browser
+//
+//  Created by Djuro Alfirevic on 3/10/17.
+//  Copyright © 2017 Djuro Alfirevic. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+#pragma mark - UIApplicationDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
