8a0b3fcf988319e0acdb584e909014a2ec72568b
Djuro Alfirevic <djuro.alfirevic@symphony.is>
2017-03-25 12:15:13 +0100

    MyPlayground added

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+// #import <UIKit/UIKit.h>
+import UIKit
+
+// NSString *str = @"Hello, playground";
+let str = "Hello, playground"
+
+// NSString *text = [NSString stringWithFormat:@"Moje ime je Djuro i imam %d godine", 34];
+let text = "Moje ime je Djuro i imam \(34) godine"
+
+let broj = 34
+let daLiJeSunceNapolju = false
+
+// @property (weak, nonatomic) IBOutlet UIButton *someButton;
+// @IBOutlet weak var someButton: UIButton!
+
+// CGSize size = CGSizeMake(width: 10, height: 10)
+let size = CGSize(width: 10, height: 10)
+
+// NSDate *date = [[NSDate alloc] init];
+let date = Date()
+
+// Optionals
+var temperature: Double?
+
+//print("\(temperature!)")
