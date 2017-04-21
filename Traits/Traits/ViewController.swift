dc7878859632ec2507486f6b2ed007961f7a775c
Djuro Alfirevic <djuro.alfirevic@symphony.is>
2017-04-21 21:05:19 +0200

    Traits project added

---
+++ Traits/Traits/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Traits
+//
+//  Created by Djuro Alfirevic on 4/21/17.
+//  Copyright © 2017 Sistemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
